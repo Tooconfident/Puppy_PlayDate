e7c8bebe6c5c5da1981d8100c7700f26af9c930f
Carlos Gonzalez <cyberpolin@gmail.com>
2016-06-03 20:13:50 -0700

    Add validations to models Dog, Photo, Playdate, User

--- puppy_play_date/app/models/dog.rb
+++ puppy_play_date/app/models/dog.rb
@@ -3,4 +3,6 @@ class Dog < ActiveRecord::Base
   has_many :playdates, through: :memberships
   belongs_to :owner, class_name: "User", foreign_key: "user_id"
   has_one :photo
+
+  validates :name, :breed, :age, presence: true
 end
--- puppy_play_date/app/models/user.rb
+++ puppy_play_date/app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
   has_many :dogs
   has_many :playdates
+
+  validates :username, :email, :name, presence: true
+  validates :email, uniqueness: true
 end
