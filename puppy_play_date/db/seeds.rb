b612c774011423f59f74f5ef63e093b2a55aed2d
Carlos Gonzalez <cyberpolin@gmail.com>
2016-06-03 18:23:54 -0700

    Add basic seeds file with fake users, dogs, playdates, memberships

--- puppy_play_date/db/seeds.rb
+++ puppy_play_date/db/seeds.rb
@@ -5,3 +5,60 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Membership.delete_all
+Playdate.delete_all
+Dog.delete_all
+User.delete_all
+
+# Create fake users
+24.times do
+  user = User.create(
+    name: Faker::Name.name,
+    username: Faker::Internet.user_name,
+    email: Faker::Internet.email,
+    #password: '123'
+  )
+
+  # Create fake dogs
+  rand(0..10).times do
+    Dog.create(
+      name: Faker::Name.name,
+      breed: Faker::StarWars.specie,
+      age: rand(3..20),
+      toy: Faker::Commerce.product_name,
+      description: Faker::Lorem.paragraph,
+      gender: ["male", "female"].sample,
+      owner: user
+    )
+  end
+end
+
+4.times do
+  Playdate.create(
+    name: Faker::Team.name,
+    description: Faker::Lorem.paragraph,
+    location: "1, 2",
+    frequency: [nil, "daily", "weekly", "monthly"].sample,
+    time_day: "#{Date::DAYNAMES.sample} at #{rand(0..24)}",
+    organizer: User.all.sample
+  )
+end
+
+12.times do
+  Membership.create(
+    dog: Dog.all.sample,
+    playdate: Playdate.all.sample
+  )
+end
+
+carlos = User.create(
+  name: "Carlos",
+  username: "carlos",
+)
+
+Dog.create(
+  name: "Macy Gray",
+  breed: "Schnauzer",
+  user: carlos
+)
