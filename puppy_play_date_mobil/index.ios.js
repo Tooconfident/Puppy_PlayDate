cdc69bc541aed276aa1d66e825d9b17935afaf9c
Carlos Gonzalez <cyberpolin@gmail.com>
2016-06-03 23:29:03 -0700

    Start new mobile app

---
+++ puppy_play_date_mobil/index.ios.js
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+var styles = require('./style.js')
+class puppy_play_date_mobil extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('puppy_play_date_mobil', () => puppy_play_date_mobil);
