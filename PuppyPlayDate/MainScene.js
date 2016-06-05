6a885ed47513ac4de9e0025b31890168802d1798
Carlos Gonzalez <cyberpolin@gmail.com>
2016-06-04 19:13:17 -0700

    Add basic dog button

--- PuppyPlayDate/MainScene.js
+++ PuppyPlayDate/MainScene.js
@@ -4,7 +4,8 @@ import {
   StyleSheet,
   Text,
   View,
-  Navigator
+  Navigator,
+  TouchableHighlight
 } from 'react-native';
 
 class MainScene extends Component {
@@ -12,10 +13,32 @@ class MainScene extends Component {
     return(
       <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
         <Text>Hola a todos!</Text>
+        <TouchableHighlight style={styles.button}>
+          <Text style={styles.buttonText}>
+            Dogs
+          </Text>
+        </TouchableHighlight>
       </View>
 
     );
   }
 }
+
+const styles = StyleSheet.create({
+  button: {
+    height: 36,
+    backgroundColor: "#48bbec",
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: "stretch",
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "white",
+    alignSelf: "center",
+  }
+});
+
 
 module.exports = MainScene;
