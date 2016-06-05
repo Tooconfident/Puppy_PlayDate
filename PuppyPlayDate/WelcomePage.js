6c0002f2a62a960ae25df67ba8e4a83bd3408c4d
Carlos Gonzalez <cyberpolin@gmail.com>
2016-06-05 12:18:53 -0700

    Add skeleton for all the remaining app components

---
+++ PuppyPlayDate/WelcomePage.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  ListView,
+  Navigator,
+  Image,
+  TouchableHighlight
+} from 'react-native';
+
+class WelcomePage extends Components {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>
+          This is Welcome Page
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 14,
+  }
+});
+
+module.exports = WelcomePage;
