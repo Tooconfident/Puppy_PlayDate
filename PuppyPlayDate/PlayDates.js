a38b176d336143ab19a7e60b893a54fd4ae71c3a
Buck & Renan Martins <pair+buckmelton.nbkhope@devbootcamp.com>
2016-06-05 15:24:37 -0700

    Define PlayDates component with GET request to backend API

--- PuppyPlayDate/PlayDates.js
+++ PuppyPlayDate/PlayDates.js
@@ -1,28 +1,114 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View,
+  Image,
+  ListView,
   Navigator,
+  StyleSheet,
   TabBarIOS,
-  Image
+  Text,
+  TouchableHighlight,
+  View
 } from 'react-native';
 
+var REQUEST_URL = 'http://localhost:3000/playdates';
+
 class PlayDates extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      dataSource : new ListView.DataSource(
+        {rowHasChanged: (r1, r2) => r1 !== r2}
+      ),
+      loaded: false,
+    };
+  }
+
+  componentDidMount(){
+    this.fetchData();
+  }
+
+  fetchData(){
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData),
+          loaded: true,
+        });
+      })
+      .done();
+  }
+
+  renderRow(rowData, sectionID, rowID){
+    return(
+      <TouchableHighlight>
+        <View style={styles.rowContainer}>
+          <View style={styles.textContainer}>
+            <Text style={styles.title}>
+              {rowData.name}
+            </Text>
+            <Text>
+              <Text style={styles.subtitle}>Location:</Text> {rowData.location}
+              {', '}
+              <Text style={styles.subtitle}>Day and Time:</Text> {rowData.time_day}
+            </Text>
+            <Text>
+              <Text style={styles.subtitle}>Description:</Text> {rowData.description}
+            </Text>
+          </View>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+
   render() {
+    if (!this.state.loaded){
+      return(<Text>Loading...</Text>)
+    }
     return (
-      <Text style={styles.center}>group index png</Text>
+      <View style={styles.container}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow.bind(this)}
+        />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  center: {
+  container: {
     flex: 1,
-    textAlign: 'center'
-  }
-})
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    borderWidth: 2,
+    borderRadius: 12,
+    padding: 10,
+    backgroundColor: 'antiquewhite'
+  },
+  textContainer: {
+    flex: 1,
+    flexWrap: 'wrap',
+  },
+  rowContainer: {
+    flexDirection: 'column',
+    padding: 10,
+    borderBottomWidth: 2,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  subtitle: {
+    fontWeight: 'bold',
+    fontSize: 14,
+  },
+});
 
 
 module.exports = PlayDates;
