b065aba2d1e130e347413eb499e2ea5dd8b52974
Carlos Gonzalez & Ryan Wilkins <pair+cyberpolin.tooconfident@devbootcamp.com>
2016-06-05 16:52:38 -0700

    Add UserEdit.js

--- PuppyPlayDate/UserEdit.js
+++ PuppyPlayDate/UserEdit.js
@@ -3,6 +3,7 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  TextInput,
   View,
   ListView,
   Navigator,
@@ -10,13 +11,35 @@ import {
   TouchableHighlight
 } from 'react-native';
 
-class DogProfile extends Components {
+class UserEdit extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.text}>
-          This is Dog Profile
+          Edit User
         </Text>
+        <TextInput
+          placeholder="Username"
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Name"
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Email"
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          style={styles.input}
+        />
+        <TouchableHighlight
+          style={styles.button}>
+          <Text style={styles.buttonText}>
+            Edit
+          </Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -29,8 +52,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
-    fontSize: 14,
+    fontSize: 40,
+  },
+  button: {
+    height: 36,
+    backgroundColor: "#48bbec",
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: "stretch",
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "white",
+    alignSelf: "center",
+  },
+  input: {
+    height: 40,
   }
 });
 
-module.exports = DogProfile;
+module.exports = UserEdit;
