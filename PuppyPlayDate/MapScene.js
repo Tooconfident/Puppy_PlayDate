492dc4d281aab45d773cb75bc6f1406209134f48
Ryan Wilkins <email": null>
2016-06-06 15:41:57 -0700

    Add more map functionality to experiment with

--- PuppyPlayDate/MapScene.js
+++ PuppyPlayDate/MapScene.js
@@ -42,7 +42,6 @@ class MapScene extends Component {
             latitude: LATITUDE + SPACE,
             longitude: LONGITUDE + SPACE,
           },
-          color: '#FFFFFF',
         },
         {
           coordinate: {
@@ -71,18 +70,50 @@ class MapScene extends Component {
   render() {
     const { region, markers } = this.state;
     return (
-      <MapView
-        style={styles.map}
-        initialRegion={region}
-        showsUserLocation= {true}
-        followsUserLocation= {true}
-        showsPointsOfInterest= {false}>
-        <MapView.Marker
-          coordinate={markers[0].coordinate}
-          title="PlayDate Title"
-          description="Brief Description"
-        />
-      </MapView>
+      <View>
+        <MapView
+          style={styles.map}
+          initialRegion={region}
+          showsUserLocation= {true}
+          followsUserLocation= {false}
+          showsPointsOfInterest= {false}
+          // onMarkerPress= show event // callback function
+          // onMarkSelect= go to group page // these two may have to be flipped
+          >
+          <MapView.Marker
+            ref="m1"
+            coordinate={markers[0].coordinate}
+            title="PlayDate Title"
+            description="Brief Description"
+            pinColor=""
+          />
+          <MapView.Marker ref="m2" coordinate={markers[1].coordinate}>
+            <MapView.Callout>
+              <View>
+                <Text>This is a plain view</Text>
+              </View>
+            </MapView.Callout>
+          </MapView.Marker>
+          <MapView.Marker
+            ref="m3"
+            coordinate={markers[2].coordinate}
+            calloutOffset={{ x: -8, y: 28 }}
+            calloutAnchor={{ x: 0.5, y: 0.4 }}
+          >
+            <MapView.Callout tooltip>
+              <Text style={{ color: '#fff', backgroundColor: 'black' }}>This is a custom callout bubble view</Text>
+            </MapView.Callout>
+          </MapView.Marker>
+        </MapView>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={this.show} style={[styles.bubble, styles.button]}>
+            <Text>Show</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.hide} style={[styles.bubble, styles.button]}>
+            <Text>Hide</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
   )};
   onRegionChangeComplete(region) {
     console.log(region);
@@ -91,11 +122,38 @@ class MapScene extends Component {
 
 const styles = StyleSheet.create({
   map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
     flex: 1,
     width: width,
     height: height,
     borderWidth: 1,
-  }
+  },
+  bubble: {
+    flex: 1,
+    backgroundColor: 'rgba(255,255,255,0.7)',
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,
+  },
+  latlng: {
+    width: 200,
+    alignItems: 'stretch',
+  },
+  button: {
+    width: 80,
+    paddingHorizontal: 12,
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginVertical: 60,
+    backgroundColor: 'transparent',
+  },
 });
 
 
