e4a4bac5c74acff2c6348d60b8bba94de91d99af
Renan Martins <email": null>
2016-06-06 10:01:35 -0700

    Define PlayDateEdit component to edit playdate

--- PuppyPlayDate/PlayDateEdit.js
+++ PuppyPlayDate/PlayDateEdit.js
@@ -7,16 +7,64 @@ import {
   ListView,
   Navigator,
   Image,
-  TouchableHighlight
+  TouchableHighlight,
+  TextInput,
 } from 'react-native';
 
-class DogProfile extends Components {
+
+
+class PlayDateEdit extends Component {
+
+  constructor(props) {
+    super(props);
+
+    // Initialize Playdate Attributes
+    this.state = {
+      name: "",
+      location: "",
+      timeDay: "",
+      description: "",
+    };
+  }
+
+  onPressEdit() {
+    this.props.navigator.pop();
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>
-          This is Dog Profile
-        </Text>
+        <Text style={styles.inputLabel}>Playdate Name</Text>
+        <TextInput
+          style={styles.inputText}
+          value={this.state.name}
+          onChangeText={(text) => this.setState({name: text})} />
+
+        <Text style={styles.inputLabel}>Playdate Location</Text>
+        <TextInput
+          style={styles.inputText}
+          value={this.state.location}
+          onChangeText={(text) => this.setState({location: text})} />
+
+        <Text style={styles.inputLabel}>Playdate Date and Time</Text>
+        <TextInput
+          style={styles.inputText}
+          value={this.state.timeDay}
+          onChangeText={(text) => this.setState({timeDay: text})} />
+
+        <Text style={styles.inputLabel}>Playdate Description</Text>
+        <TextInput
+          style={styles.inputText}
+          value={this.state.description}
+          onChangeText={(text) => this.setState({description: text})} />
+
+        <TouchableHighlight
+          style={styles.button}
+          onPress={this.onPressEdit.bind(this)}
+          underlayColor='#99d9f4'
+        >
+          <Text style={styles.buttonText}>Edit</Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -30,7 +78,29 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 14,
-  }
+  },
+  button: {
+    borderWidth: 2,
+    borderRadius: 12,
+    padding: 10,
+    backgroundColor: 'antiquewhite'
+  },
+  inputLabel: {
+    fontWeight: 'bold',
+  },
+  inputText: {
+    height: 30,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 16,
+    padding: 10,
+    backgroundColor: '#EBFAFF',
+    marginBottom: 10,
+  },
+  textArea: {
+    height: 100,
+  },
+
 });
 
-module.exports = DogProfile;
+module.exports = PlayDateEdit;
