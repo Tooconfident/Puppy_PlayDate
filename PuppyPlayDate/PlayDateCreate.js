f72fff958937467b88d0aecc993392d8e733eed1
Buck & Renan Martins <pair+buckmelton.nbkhope@devbootcamp.com>
2016-06-05 20:46:15 -0700

    Update PlayDateCreate to handle TextInput persistence of user input

--- PuppyPlayDate/PlayDateCreate.js
+++ PuppyPlayDate/PlayDateCreate.js
@@ -19,7 +19,16 @@ import UserDogs from './UserDogs';
 class PlayDateCreate extends Component {
   constructor(props) {
     super(props);
-    this.state = { text: "" }
+    this.state = {
+      text: "",
+      playdate: {
+        name: "",
+        location: "",
+        time_day: "",
+        description: "",
+        frequency: "",
+      },
+    }
   }
 
   createGroupPressed() {
@@ -31,7 +40,37 @@ class PlayDateCreate extends Component {
     console.log("PlayDateCreate: componentDidMount: " + this.props);
   }
 
+  onInputChangeName(event) {
+    this.setState({
+      playdate: { name: event.nativeEvent.name },
+    });
+  }
+
+  onInputChangeLocation(event) {
+    this.setState({
+      playdate: { location: event.nativeEvent.location },
+    });
+  }
+
+  onInputChangeTimeDay(event) {
+    // this.setState({
+    //   playdate: { time_day: event.nativeEvent.time_day },
+    // });
+    this.state.playdate.time_day = event.nativeEvent.time_day;
+  }
+
+  onInputChangeDescription(event) {
+    this.setState({
+      text: event.nativeEvent.description,
+      height: event.nativeEvent.contentSize.height,
+      //playdate: { description: event.nativeEvent.description },
+    });
+    this.state.playdate.description = event.nativeEvent.description;
+  }
+
   render() {
+    var playdate = this.state.playdate;
+
     return (
       <View style={styles.container}>
         <Text style={styles.pageTitle}>
@@ -40,32 +79,46 @@ class PlayDateCreate extends Component {
         <TextInput
           style={styles.inputText}
           placeholder="Name"
+          value={playdate.name}
+          onChange={this.onInputChangeName.bind(this)}
         />
         <TextInput
           style={styles.inputText}
           placeholder="Location"
+          value={playdate.location}
+          onChange={this.onInputChangeLocation.bind(this)}
         />
         <TextInput
           style={styles.inputText}
           placeholder="Time & Day of the Week"
+          value={playdate.time_day}
+          onChange={this.onInputChangeTimeDay.bind(this)}
         />
         <TextInput
           style={[styles.inputText, styles.textArea]}
           placeholder="Description"
           multiline={true}
-          onChange={(event) => {
-            this.setState({
-              text: event.nativeEvent.text,
-              height: event.nativeEvent.contentSize.height,
-            });
-          }}
-          value={this.state.text}
+          onChange={this.onInputChangeDescription.bind(this)}
+          value={playdate.description}
         />
-        <Picker>
+        <Picker
+          selectedValue={playdate.frequency}>
           <Picker.Item label="daily" value="daily" />
           <Picker.Item label="weekly" value="weekly" />
           <Picker.Item label="monthly" value="monthly" />
         </Picker>
+        <View>
+          <Text>Debugger:</Text>
+          <Text>
+            {playdate.name}
+            {playdate.location}
+            {playdate.description}
+            {playdate.time_day}
+            {playdate.frequency}
+
+          </Text>
+        </View>
+
         <TouchableHighlight
           style={styles.button}
           onPress={() => this.createGroupPressed()}>
