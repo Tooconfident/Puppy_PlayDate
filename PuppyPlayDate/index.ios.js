d6fdbf39e3baad9696e2da46b5ea6b867a5c1b76
Carlos Gonzalez & Renan Martins <pair+cyberpolin.nbkhope@devbootcamp.com>
2016-06-07 11:45:57 -0700

    Replace Navigator with NavigatorIOS; import Login component; comment out unnecessary functions

--- PuppyPlayDate/index.ios.js
+++ PuppyPlayDate/index.ios.js
@@ -7,63 +7,51 @@
 import React, { Component } from 'react';
 import {
  AppRegistry,
- Navigator,
- AsyncStorage,
+ NavigatorIOS,
+ StyleSheet,
 } from 'react-native';
 
-
-import MapScene from './MapScene';
+import Login from './Login';
 import MainScene from './MainScene';
+import MapScene from './MapScene';
 import WelcomePage from './WelcomePage';
 
-const styles = require('./style.js')
-
+let styles = StyleSheet.create({
+  wrapper: {
+    flex: 1
+  }
+});
 class PuppyPlayDateApp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  componentDidMount() {
-        // AsyncStorage.clear()
-        AsyncStorage.getItem("userID").then((value) => {
-          console.log('current.val '+ value);
-            this.setState({"userID": value});
-        }).done();
-    }
-  renderScene(route, navigator){
-    console.log("renderScene was called: passProps: " + route.passProps);
-    return(
-      <route.component navigator={navigator} {...route.passProps}/>
-    );
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {};
+  // }
+  //
+  // componentDidMount() {
+  //   AsyncStorage.clear()
+  //   AsyncStorage.getItem("userID").then((value) => {
+  //     console.log('current.val '+ value);
+  //       this.setState({"userID": value});
+  //   }).done();
+  // }
+  //
+  // renderScene(route, navigator){
+  //   console.log("renderScene was called: passProps: " + route.passProps);
+  //   return(
+  //     <route.component navigator={navigator} {...route.passProps}/>
+  //   );
+  // }
+  //
+  //
   render() {
-
-    if (this.state.userID != undefined){
-      return (<MainScene/>);
-    } else {
-      return (
-        <Navigator
-        sceneStyle={styles.container}
-        ref={(navigator) => { this.navigator = navigator; }}
-        renderScene={this.renderScene}
-        tintColor='#D6573D'
-        barTintColor='#FFFFFD'
-        titleTextColor='#D6573D'
-        navigationBarHidden={true}
-        initialRoute={{
-          title: 'Main',
-          component: WelcomePage,
-        }}
-      >
+    return (
+      <NavigatorIOS
+        style={styles.wrapper}
+        initialRoute= {{
+          title: 'Login Page',
+          component: Login,
+        }}/>
       );
-
-        <WelcomePage/>
-        </Navigator>
-      );
-    }
-
   }
-
-
 }
 AppRegistry.registerComponent('PuppyPlayDate', () => PuppyPlayDateApp);
