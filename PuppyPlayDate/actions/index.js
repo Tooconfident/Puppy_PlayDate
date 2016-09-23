67d5c0d4cadb647acfbfe2466365ba47051b2304
nbkhope <ryumemaru@yahoo.com>
2016-09-22 23:12:56 -0700

    Update actions to use axios instead of fetch; add FETCH_USER, FETCH_PLAYDATES, FETCH_PLAYDATE constants; add fetchUser() action creator

--- PuppyPlayDate/actions/index.js
+++ PuppyPlayDate/actions/index.js
@@ -1,6 +1,11 @@
+import axios from 'axios';
+
 // Constants to define action type
 export const FETCH_DOGS = 'FETCH_DOGS';
 export const FETCH_DOG = 'FETCH_DOG';
+export const FETCH_USER = 'FETCH_USER';
+export const FETCH_PLAYDATES = 'FETCH_PLAYDATES';
+export const FETCH_PLAYDATE = 'FETCH_PLAYDATE';
 
 const REQUEST_URL = 'http://localhost:3000';
 
@@ -15,11 +20,20 @@ export function fetchDogs(userId) {
 }
 
 export function fetchDog(id) {
-  const request = fetch(`${REQUEST_URL}/dogs/${id}`);
+  const request = axios(`${REQUEST_URL}/dogs/${id}`);
 
   // return the action
   return {
     type: FETCH_DOG,
     payload: request // redux-promise will take this and replace with the resolved promise
+  };
+}
+
+export function fetchUser(id) {
+  const request = axios(`${REQUEST_URL}/users/${id}`);
+
+  return {
+    type: FETCH_USER,
+    payload: request
   };
 }
