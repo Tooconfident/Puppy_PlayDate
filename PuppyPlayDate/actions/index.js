02841cbef77ae741d88be06e33f821b2ff3dd827
nbkhope <ryumemaru@yahoo.com>
2016-09-23 14:41:44 -0700

    Add more actions and action creators; now fetchPlaydates will retrieve all playdates and fetchUserPlaydates will retrieve all the playdates a user owns

--- PuppyPlayDate/actions/index.js
+++ PuppyPlayDate/actions/index.js
@@ -3,9 +3,14 @@ import axios from 'axios';
 // Constants to define action type
 export const FETCH_DOGS = 'FETCH_DOGS';
 export const FETCH_DOG = 'FETCH_DOG';
+export const CREATE_DOG = 'CREATE_DOG';
+export const UPDATE_DOG = 'UPDATE_DOG';
 export const FETCH_USER = 'FETCH_USER';
 export const FETCH_PLAYDATES = 'FETCH_PLAYDATES';
+export const FETCH_USER_PLAYDATES = 'FETCH_USER_PLAYDATES';
 export const FETCH_PLAYDATE = 'FETCH_PLAYDATE';
+export const CREATE_PLAYDATE = 'CREATE_PLAYDATE';
+export const UPDATE_PLAYDATE = 'UPDATE_PLAYDATE';
 
 const REQUEST_URL = 'http://localhost:3000';
 
@@ -20,7 +25,7 @@ export function fetchDogs(userId) {
 }
 
 export function fetchDog(id) {
-  const request = axios(`${REQUEST_URL}/dogs/${id}`);
+  const request = axios.get(`${REQUEST_URL}/dogs/${id}`);
 
   // return the action
   return {
@@ -29,8 +34,26 @@ export function fetchDog(id) {
   };
 }
 
+export function createDog(dog) {
+  const request = axios.post(`${REQUEST_URL}/dogs`, dog);
+
+  return {
+    type: CREATE_DOG,
+    payload: request
+  };
+}
+
+export function updateDog(dog) {
+  const request = axios.patch(`${REQUEST_URL}/dogs/${dog.id}`, dog);
+
+  return {
+    type: UPDATE_DOG,
+    payload: request
+  };
+}
+
 export function fetchUser(id) {
-  const request = axios(`${REQUEST_URL}/users/${id}`);
+  const request = axios.get(`${REQUEST_URL}/users/${id}`);
 
   return {
     type: FETCH_USER,
@@ -38,8 +61,8 @@ export function fetchUser(id) {
   };
 }
 
-export function fetchPlaydates(userId) {
-  const request = axios(`${REQUEST_URL}/playdates?user_id=${userId}`);
+export function fetchPlaydates() {
+  const request = axios.get(`${REQUEST_URL}/playdates`);
 
   return {
     type: FETCH_PLAYDATES,
@@ -47,11 +70,38 @@ export function fetchPlaydates(userId) {
   };
 }
 
+export function fetchUserPlaydates(userId) {
+  const request = axios.get(`${REQUEST_URL}/users/${userId}/playdates`);
+
+  return {
+    type: FETCH_USER_PLAYDATES,
+    payload: request
+  };
+}
+
 export function fetchPlaydate(id) {
-  const request = axios(`${REQUEST_URL}/playdates/${id}`);
+  const request = axios.get(`${REQUEST_URL}/playdates/${id}`);
 
   return {
     type: FETCH_PLAYDATE,
     payload: request
   };
+}
+
+export function createPlaydate(playdate) {
+  const request = axios.post(`${REQUEST_URL}/playdates`, playdate);
+
+  return {
+    type: CREATE_PLAYDATE,
+    payload: request
+  };
+}
+
+export function updatePlaydate(playdate) {
+  const request = axios.patch(`${REQUEST_URL}/playdates/${playdate.id}`, playdate);
+
+  return {
+    type: UPDATE_PLAYDATE,
+    payload: request
+  }
 }
