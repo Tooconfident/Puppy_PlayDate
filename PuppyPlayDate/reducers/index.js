653659466adc7ab7f549df2280567aa3f99d5d9a
nbkhope <ryumemaru@yahoo.com>
2016-09-22 23:43:15 -0700

    Add new reducers to combine list

--- PuppyPlayDate/reducers/index.js
+++ PuppyPlayDate/reducers/index.js
@@ -2,11 +2,15 @@ import { combineReducers } from 'redux';
 
 // Custom Reducers
 import DogsReducer from './reducer_dogs';
+import PlaydatesReducer from './reducer_playdates';
+import UsersReducer from './reducer_users';
 
 // Combine all reducers here
 // (don't forget to assign key-value pairs)
 const rootReducer = combineReducers({
   dogs: DogsReducer,
+  playdates: PlaydatesReducer,
+  users: UsersReducer
 });
 
 export default rootReducer;
