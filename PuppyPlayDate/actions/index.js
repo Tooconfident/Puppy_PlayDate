d61ab85d06079bb4eb425b1305ab086fd1b3262e
nbkhope <ryumemaru@yahoo.com>
2016-09-30 14:55:35 -0700

    Move all action types into types.js

--- PuppyPlayDate/actions/index.js
+++ PuppyPlayDate/actions/index.js
@@ -1,16 +1,7 @@
 import axios from 'axios';
 
-// Constants to define action type
-export const FETCH_DOGS = 'FETCH_DOGS';
-export const FETCH_DOG = 'FETCH_DOG';
-export const CREATE_DOG = 'CREATE_DOG';
-export const UPDATE_DOG = 'UPDATE_DOG';
-export const FETCH_USER = 'FETCH_USER';
-export const FETCH_PLAYDATES = 'FETCH_PLAYDATES';
-export const FETCH_USER_PLAYDATES = 'FETCH_USER_PLAYDATES';
-export const FETCH_PLAYDATE = 'FETCH_PLAYDATE';
-export const CREATE_PLAYDATE = 'CREATE_PLAYDATE';
-export const UPDATE_PLAYDATE = 'UPDATE_PLAYDATE';
+// Import all action types
+export * from './types';
 
 const REQUEST_URL = 'http://localhost:3000';
 
---
+++ PuppyPlayDate/actions/types.js
@@ -0,0 +1,11 @@
+// Constants to define action type
+export const FETCH_DOGS = 'FETCH_DOGS';
+export const FETCH_DOG = 'FETCH_DOG';
+export const CREATE_DOG = 'CREATE_DOG';
+export const UPDATE_DOG = 'UPDATE_DOG';
+export const FETCH_USER = 'FETCH_USER';
+export const FETCH_PLAYDATES = 'FETCH_PLAYDATES';
+export const FETCH_USER_PLAYDATES = 'FETCH_USER_PLAYDATES';
+export const FETCH_PLAYDATE = 'FETCH_PLAYDATE';
+export const CREATE_PLAYDATE = 'CREATE_PLAYDATE';
+export const UPDATE_PLAYDATE = 'UPDATE_PLAYDATE';
