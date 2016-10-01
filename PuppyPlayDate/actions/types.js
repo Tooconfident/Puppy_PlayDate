06448e4ebd4cf67bbd2aabb1c60e036ef7ad1098
nbkhope <ryumemaru@yahoo.com>
2016-10-01 13:39:38 -0700

    Add commented out action types for NAV

--- PuppyPlayDate/actions/types.js
+++ PuppyPlayDate/actions/types.js
@@ -12,3 +12,5 @@ export const UPDATE_PLAYDATE = 'UPDATE_PLAYDATE';
 export const AUTH_USER = 'AUTH_USER';
 export const UNAUTH_USER = 'UNAUTH_USER';
 export const AUTH_ERROR = 'AUTH_ERROR';
+// export const SET_NAV = 'SET_NAV';
+// export const SET_ROUTE = 'SET_ROUTE';
