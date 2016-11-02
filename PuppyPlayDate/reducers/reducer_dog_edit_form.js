1f58a0feb998b763500641bf7767e8175db49861
nbkhope <ryumemaru@yahoo.com>
2016-11-02 16:14:30 -0700

    Add DogEditFormReducer and action types

---
+++ PuppyPlayDate/reducers/reducer_dog_edit_form.js
@@ -0,0 +1,24 @@
+import {
+  EDIT_DOG_STATE_UPDATE,
+  EDIT_DOG_SUCCESS
+} from '../actions/types';
+
+const INITIAL_STATE = {
+  name: '',
+  breed: '',
+  age: '',
+  toy: '',
+  avatar: '',
+  gender: '',
+  description: '',
+};
+
+export default function(state = INITIAL_STATE, action) {
+  case EDIT_DOG_STATE_UPDATE:
+    return { ...state, ...action.payload };
+  case EDIT_DOG_SUCCESS:
+    // this might be unnecessary
+    return INITIAL_STATE;
+  default:
+    return state;
+}
